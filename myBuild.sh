9274e6d4811af61e68fe222230c56e262fa8d4be
Stijn.Asnong <Stijn.asnong@oakstreethealth.com>
2021-06-21 10:28:46 -0500

    add test to build

--- myBuild.sh
+++ myBuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update -y && apt-get -y install xvfb libgtk-3-0 libxss1 libasound2 && vue-cli-service build && npm run test:unit && npm run test:e2e
+apt-get update -y && apt-get -y install xvfb libgtk-3-0 libxss1 libasound2 && vue-cli-service build && npm run test:unit && npm run test:e2e-headless
